e4e82542ae32d43a3d0b8c086e184c5355fbbc9a
darius <darius@66-214-130-73.dhcp.gldl.ca.charter.com>
2008-08-11 16:18:25 -0700

    more 16-wide gates

--- gates.py
+++ gates.py
@@ -81,6 +81,40 @@ def not16(in_):
     return tuple(~w for w in in_)
 
 
+def test_and16():
+    a = wires(16)
+    b = wires(16)
+    out = and16(a, b)
+    simtest.test(locals(), 'tests/1/And16.tst')
+
+def and16(a, b):
+    "16-wide AND"
+    return tuple(ai & bi for ai, bi in zip(a, b))
+
+
+def test_or16():
+    a = wires(16)
+    b = wires(16)
+    out = or16(a, b)
+    simtest.test(locals(), 'tests/1/Or16.tst')
+
+def or16(a, b):
+    "16-wide OR"
+    return tuple(ai | bi for ai, bi in zip(a, b))
+
+
+def test_mux16():
+    a = wires(16)
+    b = wires(16)
+    sel = Wire()
+    out = mux16(a, b, sel)
+    simtest.test(locals(), 'tests/1/Mux16.tst')
+
+def mux16(a, b, sel):
+    "16-wide 2-way MUX"
+    return tuple(mux(ai, bi, sel) for ai, bi in zip(a, b))
+
+
 def main():
     for name, value in globals().items():
         if name.startswith('test'):
