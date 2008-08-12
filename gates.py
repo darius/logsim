071d0fac262a4d538984a3bf69aa48ee696f3f8b
darius <darius@66-214-130-73.dhcp.gldl.ca.charter.com>
2008-08-11 17:20:39 -0700

    add multiway gates like or8way

--- gates.py
+++ gates.py
@@ -115,6 +115,83 @@ def mux16(a, b, sel):
     return tuple(mux(ai, bi, sel) for ai, bi in zip(a, b))
 
 
+def test_or8way():
+    in_ = wires(8)
+    out = or8way(in_)
+    simtest.test(locals(), 'tests/1/Or8way.tst')
+
+def or8way(in_):
+    assert 8 == len(in_)
+    w1 = [in_[i] | in_[i+1] for i in range(0, 8, 2)]
+    w2 = [w1[i] | w1[i+1] for i in range(0, 4, 2)]
+    return w2[0] | w2[1]
+
+
+def test_mux4way16():
+    a = wires(16)
+    b = wires(16)
+    c = wires(16)
+    d = wires(16)
+    sel = wires(2)
+    out = mux4way16(a, b, c, d, sel)
+    simtest.test(locals(), 'tests/1/Mux4way16.tst')
+
+def mux4way16(a, b, c, d, sel):
+    assert (16, 16, 16, 16) == (len(a), len(b), len(c), len(d))
+    assert 2 == len(sel)
+    return mux16(mux16(a, b, sel[1]),
+                 mux16(c, d, sel[1]),
+                 sel[0])
+
+
+def test_mux8way16():
+    a = wires(16)
+    b = wires(16)
+    c = wires(16)
+    d = wires(16)
+    e = wires(16)
+    f = wires(16)
+    g = wires(16)
+    h = wires(16)
+    sel = wires(3)
+    out = mux8way16(a, b, c, d, e, f, g, h, sel)
+    simtest.test(locals(), 'tests/1/Mux8way16.tst')
+
+def mux8way16(a, b, c, d, e, f, g, h, sel):
+    assert [16] * 8 == map(len, [a, b, c, d, e, f, g, h])
+    assert 3 == len(sel)
+    return mux16(mux4way16(a, b, c, d, sel[1:]),
+                 mux4way16(e, f, g, h, sel[1:]),
+                 sel[0])
+
+
+def test_dmux4way():
+    in_ = Wire()
+    sel = wires(2)
+    a, b, c, d = dmux4way(in_, sel)
+    simtest.test(locals(), 'tests/1/DMux4way.tst')
+
+def dmux4way(in_, sel):
+    "if sel=00 then (in,0,0,0) else ... if sel=11 then (0,0,0,in)"
+    assert 2 == len(sel)
+    w0, w1 = dmux(in_, sel[0])
+    return dmux(w0, sel[1]) + dmux(w1, sel[1])
+
+
+def test_dmux8way():
+    in_ = Wire()
+    sel = wires(3)
+    a, b, c, d, e, f, g, h = dmux8way(in_, sel)
+    simtest.test(locals(), 'tests/1/DMux8way.tst')
+
+def dmux8way(in_, sel):
+    """if sel=000 then (in,0,0,0,0,0,0,0) else ...
+       if sel=111 then (0,0,0,0,0,0,0,in)"""
+    assert 3 == len(sel)
+    w0, w1 = dmux(in_, sel[0])
+    return dmux4way(w0, sel[1:]) + dmux4way(w1, sel[1:])
+
+
 def main():
     for name, value in globals().items():
         if name.startswith('test'):
